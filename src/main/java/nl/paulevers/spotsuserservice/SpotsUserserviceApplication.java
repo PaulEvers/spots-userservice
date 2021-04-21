64d3216c0080b80660946f9406fc7f76ff575241
Paul <paulevers1311@gmail.com>
2021-04-21 19:52:41 +0200

    Init

---
+++ src/main/java/nl/paulevers/spotsuserservice/SpotsUserserviceApplication.java
@@ -0,0 +1,13 @@
+package nl.paulevers.spotsuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotsUserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotsUserserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/paulevers/spotsuserservice/SpotsUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.paulevers.spotsuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotsUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
